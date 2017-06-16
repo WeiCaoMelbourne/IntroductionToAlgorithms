d4ae50718bd2b7ad37b46f22885224d7eef5d2bd
WeiCaoMelbourne <weicao.melbourne@gmail.com>
2017-06-16 12:28:18 +1000

    More ut cases

--- Ch2/ut.c
+++ Ch2/ut.c
@@ -26,21 +26,68 @@ void print_array(int* array, size_t len)
     printf("\n");
 }
 
-int main()
+void copy_array(int* array, size_t len, int* target)
 {
-    int result = 0;
-    int input1[0] = {12};
-    insert_sort(input1, sizeof(input1)/sizeof(int));
-    if (input[0] != 12)
+    if (!target)
+        return;
+
+    int i = 0;
+    for (; i < len; i ++)
+        target[i] = array[i];
+}
+
+int compare_arrays(int* array, size_t len, int* target)
+{
+    if (!target)
+        return;
+
+    int i = 0;
+    for (; i < len; i ++)
     {
-        
-        result = 1;
+        if (target[i] != array[i])
+            return 0;
     }
 
-    int input[10] = {12, 34, 4, 6, 4, 18, 64, 21, 100, 50};
-    // print_array(input, sizeof(input)/sizeof(int));
-    insert_sort(input, sizeof(input)/sizeof(int));
+    return 1;
+}
 
+int main()
+{
+    int ut_test_failure = 0;
+    int input1[1] = {12};
+    int before1[1];
+    copy_array(input1, sizeof(input1)/sizeof(int), before1);
+    insert_sort(input1, sizeof(input1)/sizeof(int));
+    if (input1[0] != 12)
+    {
+        printf("Failed to sort array with 1 item\n");
+        printf("before:\n");
+        print_array(before1, sizeof(input1)/sizeof(int));
+        printf("before:\n");
+        print_array(input1, sizeof(input1)/sizeof(int));
+        printf("\n");
+        ut_test_failure += 1;
+    }
 
+    int input2[10] = {12, 34, 4, 6, 4, 18, 64, 21, 100, 50};
+    int target2[10] = {4, 4, 6, 12, 18, 21, 34, 50, 64, 100};
+    int before2[10];
+    copy_array(input2, sizeof(input2)/sizeof(int), before2);
+    insert_sort(input2, sizeof(input2)/sizeof(int));
+    if (!compare_arrays(input2, sizeof(input2)/sizeof(int), target2))
+    {
+        printf("Failed to sort array with 1 item\n");
+        printf("before:\n");
+        print_array(before1, sizeof(input2)/sizeof(int));
+        printf("before:\n");
+        print_array(input2, sizeof(input2)/sizeof(int));
+        printf("\n");
+        ut_test_failure += 1;
+    }
+
+    if (ut_test_failure == 0)
+        printf("Success\n");
+
+    return ut_test_failure;
     // print_array(input, sizeof(input)/sizeof(int));
 }
